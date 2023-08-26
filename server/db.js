94014013230c55efdbfe91aa81b1e38fe5ac9495
Sachin <sksachin7z2@gmail.com>
2023-08-27 03:01:23 +0530

    updated atlas

--- server/db.js
+++ server/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 import {initiateplans} from "./controller/plancontroller.js";
+import dotenv from 'dotenv'
+dotenv.config()
 const connectToMongo=async()=>{
-    await mongoose.connect('mongodb://127.0.0.1:27017/richpaneltest');
+    await mongoose.connect(`mongodb+srv://sksachin7z2:${process.env.MONGO_PASSWORD}@cluster0.hlrymf6.mongodb.net/richpanel?retryWrites=true&w=majority`);
     console.log("mongo connected");
     initiateplans();
 }
